5e19d3f85968f39fd4b11136f8ea36b6da098eef
LeeTrent <Lee.Trent.1@gmail.com>
2020-06-01 08:38:38 -0500

    Adds timeout to throttle calls to movie API

--- Movies/index.js
+++ Movies/index.js
@@ -10,13 +10,27 @@ const fetchData = async (searchTerm, apiKey) => {
 };
 
 
-const searchTermInput = document.getElementById("searchTermInput");
 
-searchTermInput.addEventListener('input', (event) => {
-    console.log(`apiKeyInput.value: "${apiKeyInput.value}"`);
-    console.log(`event.target.value: "${event.target.value}"`);
-    if (apiKeyInput.value) {
-        fetchData(event.target.value, apiKeyInput.value);
+// searchTermInput.addEventListener('input', (event) => {
+//     console.log(`apiKeyInput.value: "${apiKeyInput.value}"`);
+//     console.log(`event.target.value: "${event.target.value}"`);
+//     if (apiKeyInput.value) {
+//         fetchData(event.target.value, apiKeyInput.value);
+//     }
+// });
+
+let timeoutId;
+const onSearchTermInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
     }
+    if (apiKeyInput.value) {
+        timeoutId = setTimeout( () => {
+            fetchData(event.target.value, apiKeyInput.value);
+        }, 1000);
+    }
+};
 
-});
+const searchTermInput = document.getElementById("searchTermInput");
+searchTermInput.addEventListener('input', onSearchTermInput);
+
