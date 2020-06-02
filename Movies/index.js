eb074767af6a0933fb2947fbab8c93f5d5613ad3
LeeTrent <Lee.Trent.1@gmail.com>
2020-06-01 20:24:38 -0500

    Throttles call to movie API

--- Movies/index.js
+++ Movies/index.js
@@ -9,22 +9,14 @@ const fetchData = async (searchTerm, apiKey) => {
     console.log(response.data);
 };
 
-
-
-// searchTermInput.addEventListener('input', (event) => {
-//     console.log(`apiKeyInput.value: "${apiKeyInput.value}"`);
-//     console.log(`event.target.value: "${event.target.value}"`);
-//     if (apiKeyInput.value) {
-//         fetchData(event.target.value, apiKeyInput.value);
-//     }
-// });
-
 let timeoutId;
 const onSearchTermInput = event => {
-    if (timeoutId) {
-        clearTimeout(timeoutId);
-    }
     if (apiKeyInput.value) {
+        console.log(`timeoutId: ${timeoutId}`)
+        if (timeoutId) {
+            console.log(`Clearing timeout: ${timeoutId}`);
+            clearTimeout(timeoutId);
+        }
         timeoutId = setTimeout( () => {
             fetchData(event.target.value, apiKeyInput.value);
         }, 1000);
