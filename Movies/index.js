5891e683a49da3be3280f74dcf6f49f8980ef2c5
LeeTrent <Lee.Trent.1@gmail.com>
2020-06-02 20:06:42 -0500

    Moves debounce function into utils.js

--- Movies/index.js
+++ Movies/index.js
@@ -1,4 +1,3 @@
-
 const fetchData = async (searchTerm, apiKey) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
@@ -9,18 +8,6 @@ const fetchData = async (searchTerm, apiKey) => {
     console.log(response.data);
 };
 
-const debounce = (func, delay=1000) => {
-    let timeoutId;
-    return (...args) => {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout( () => {
-            func.apply(null, args);
-        }, delay);
-    };
-};
-
 const onSearchTermInput = (event) => {
     if (apiKeyInput.value) {
         fetchData(event.target.value, apiKeyInput.value);
@@ -29,18 +16,4 @@ const onSearchTermInput = (event) => {
 
 const searchTermInput = document.getElementById("searchTermInput");
 searchTermInput.addEventListener( 'input', debounce(onSearchTermInput, 2000) );
-
-// let timeoutId;
-// const onSearchTermInput = event => {
-//     if (apiKeyInput.value) {
-//         console.log(`timeoutId: ${timeoutId}`)
-//         if (timeoutId) {
-//             console.log(`Clearing timeout: ${timeoutId}`);
-//             clearTimeout(timeoutId);
-//         }
-//         timeoutId = setTimeout( () => {
-//             fetchData(event.target.value, apiKeyInput.value);
-//         }, 1000);
-//     }
-// };
 
