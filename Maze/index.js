5412ea47c4c5a77f9aae7fe6faca59c23480986f
LeeTrent <lee.trent.1@gmail.com>
2020-07-16 08:13:12 -0500

    Handles keypresses

--- Maze/index.js
+++ Maze/index.js
@@ -197,3 +197,18 @@ const ball = Bodies.circle(
     unitLength / 4, // radius
 );
 World.add(world, ball);
+
+document.addEventListener('keydown', event => {
+    if (event.keyCode === 87) {
+        console.log("move ball up");
+    }
+    if (event.keyCode === 68) {
+        console.log("move ball right");
+    }
+    if (event.keyCode === 83) {
+        console.log("move ball down");
+    }
+    if (event.keyCode === 65) {
+        console.log("move ball left");
+    }
+});
